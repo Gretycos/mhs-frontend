331e8719f62a6d7e0a20bc685e6f5599da95e173
yaocong <rogwel@vip.qq.com>
2024-03-23 22:32:52 +0000

    new features: 1. added content in footer

--- src/component/FootBar/FootBar.jsx
+++ src/component/FootBar/FootBar.jsx
@@ -3,14 +3,15 @@
  * time: 16/03/2024 19:41
  */
 import "./FootBar.less"
-import {Layout} from "antd";
+import {Divider, Layout} from "antd";
 const {Footer} = Layout
 
 const FootBar = () => {
 
     return (
         <Footer className="foot-bar">
-            This is footbar
+            <Divider/>
+            Copyright © 2024 Hanyu Li, Yu-chu Lai, Yaocong Huang. All rights reserved.
         </Footer>
     )
 }
